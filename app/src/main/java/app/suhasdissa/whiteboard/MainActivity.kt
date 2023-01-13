6662df030a7c4f763a6dbf681b474861ed94f4c5
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-01-13 14:48:24 +0530

    Performance improvements

--- app/src/main/java/app/suhasdissa/whiteboard/MainActivity.kt
+++ app/src/main/java/app/suhasdissa/whiteboard/MainActivity.kt
@@ -3,16 +3,13 @@ package app.suhasdissa.whiteboard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import app.suhasdissa.whiteboard.ui.theme.WhiteboardAppTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,19 +18,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Scaffold(
-
-                        topBar = {
-                            TopAppBar(
-                                title = {
-                                    Text(stringResource(id = R.string.app_name))
-                                },
-                                actions = {}
-                            )
-                        }
-                    ) { paddingValues: PaddingValues ->
-                        DrawingApp(paddingValues)
-                    }
+                    DrawingApp()
                 }
             }
         }
